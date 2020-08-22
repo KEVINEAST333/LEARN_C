ec03ba61062c9e5d7dca3334d1e28ef01a7cbf85
KEVINEAST333 <1594066845@qq.com>
2020-08-22 23:36:20 +0800

    小红书笔试题练习

--- java_review/src/written_examination/XiaoHongShu.java
+++ java_review/src/written_examination/XiaoHongShu.java
@@ -1,5 +1,6 @@
 package written_examination;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class XiaoHongShu {
@@ -8,7 +9,7 @@ public class XiaoHongShu {
 
     输出例子1:
     CoronaVirus    */
-  public static void main(String[] args) {
+  public static void main1(String[] args) {
       Scanner scanner = new Scanner(System.in);
       String str = scanner.next();
       while(str.contains(")") ) {
@@ -26,4 +27,85 @@ public class XiaoHongShu {
       }
       System.out.println(str);
   }
-}
+  /*static class Treasure implements Comparator<Treasure> {
+      @Override
+      public String toString() {
+          return "Treasure{" +
+                  "a=" + a +
+                  ", b=" + b +
+                  '}';
+      }
+
+      int a;
+      int b;
+
+      @Override
+      public int compare(Treasure o1, Treasure o2) {
+          return o1.a - o2.b ;
+      }
+
+      public Treasure(int a, int b) {
+          this.a = a;
+          this.b = b;
+      }
+  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Treasure> arrayList = new ArrayList<>();
+        int result = 0;
+        int n = scanner.nextInt();
+        for (int i = 0; i < n;i++) {
+           int a = scanner.nextInt();
+           int b = scanner.nextInt();
+           Treasure treasure = new Treasure(a,b);
+           arrayList.add(treasure);
+        }
+        Collections.sort(arrayList, new Comparator<Treasure>() {
+            @Override
+            public int compare(Treasure o1, Treasure o2) {
+                return o1.a - o2.b;
+            }
+        });
+        for (int i = 0;i < n -1;i++) {
+            if((arrayList.get(i + 1).a >= arrayList.get(i + 1).a)
+                    &&(arrayList.get(i + 1).b >= arrayList.get(i + 1).b)) {
+                result++;
+            }
+        }
+        System.out.println(result);
+        System.out.println(arrayList);
+    }*/
+
+
+   // 来源：牛客网
+    //按照一个维度排序后按照另一个维度寻找最长增加子序列即可，这个是>=的比较简单一点，注意不能用O(n2)，要二分查找优化
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int[][] ans = new int[n][2];
+            for(int i=0;i<n;i++){
+                ans[i][0] = scanner.nextInt();
+                ans[i][1] = scanner.nextInt();
+            }
+            Arrays.sort(ans,(a, b)->a[0]!=b[0]?a[0]-b[0]:a[1]-b[1]);
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)arr[i] = ans[i][1];
+            System.out.println(LIS(arr));
+        }
+        public static int LIS(int[] arr){
+            int[] dp = new int[arr.length];
+            int res = 0;
+            for(int num:arr){
+                int l = 0,r = res;
+                while (l<r){
+                    int m = (l+r)/2;
+                    if(dp[m]<num)l = m+1;
+                    else r = m;
+                }
+                dp[l] = num;
+                if(l==res)res++;
+            }
+            return res;
+        }
+    }
