9c4950b365cd0b8a0835cc272c37d212fa6cb981
KEVINEAST333 <1594066845@qq.com>
2020-11-16 00:28:14 +0800

    C++练习

---
+++ code11-16/code11-16/源.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	string str = "sssss";
+	//cin >> str;
+	for ( int i = 0; i < 10; i++) {
+		cout << "hello word c++ "  + str << endl;
+	}
+	return 0;
+}
