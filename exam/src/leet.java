1752212287509a9ff7b0be671560ff78979bf6a4
KEVINEAST333 <1594066845@qq.com>
2021-03-21 16:53:38 +0800

    test git

--- exam/src/leet.java
+++ exam/src/leet.java
@@ -3,5 +3,7 @@ import java.util.HashSet;
 public class leet {
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet();
+        char s = '你';
+        System.out.println(s);
     }
 }
